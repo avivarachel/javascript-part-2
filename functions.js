699b63121ac01fc928c7243d61035e26ae71dbc0
Aviva Wolpert <wolpertaviva@gmail.com>
2015-09-14 20:22:19 +0000

    sort contacts by name length and alphabetically by e-mail

--- functions.js
+++ functions.js
@@ -15,3 +15,38 @@ function compare(a, b) {
 
 var longestString = ["hi", "hello"];
 longestString.sort();
+
+// 2. sort name by length, e-mail by alphabetical order
+
+var nameEmail = [
+    {name:"Max", email:"max.wolpert@gmail.com"},
+    {name:"Shona", email:"shona.michelle@gmail.com"},
+    {name:"Jacob", email: "jacob.emmanuel@icloud.com"},
+    {name:"Sven", email: "sven.ga.wolpert@gmail.com"},
+    {name:"Jayne", email: "jlemonartz@gmail.com"}
+]
+
+     var sortByNameLength = nameEmail.sort (function(a,b) {
+   if (a.name.length < b.name.length) {
+        return -1;
+   }
+   if (a.name.length > b.name.length) {
+       return 1;
+   }
+    // a must be equal to b
+   return 0;
+    }); // used similar syntax to first exercise. Will sort by name length
+    
+    
+ function emailAlphabetical(a, b) {
+    if (a.email > b.email) {
+        return 1;        
+    }
+    if (a.length < b.length) {
+        return -1;
+    }
+    return 0;
+};
+
+nameEmail.sort(emailAlphabetical);
+ // will sort by alphabetical order of e-mail addresses 
