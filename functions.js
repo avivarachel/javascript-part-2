432578314e9816f6e648c5c9122f59009338749d
Aviva Wolpert <wolpertaviva@gmail.com>
2015-09-14 18:54:01 +0000

    sort by longest string using Array.prototype.sort

--- functions.js
+++ functions.js
@@ -1,1 +1,17 @@
 // Let's practice functions 
+
+// 1. This function takes two strings, and returns a value that sort can use to determine which is the longest string
+
+function compare(a, b) {
+  if (a.length < b.length) {
+    return -1;
+  }
+  if (a.length > b.length) {
+    return 1;
+  }
+  // a must be equal to b
+  return 0;
+}
+
+var longestString = ["hi", "hello"];
+longestString.sort();
